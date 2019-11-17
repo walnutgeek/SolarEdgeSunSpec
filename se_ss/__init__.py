6c9c39f99e8247283128e35b4464710142fe4e67
Walunt Geek <wg@walnutgeek.com>
2019-11-17 01:44:21 -0800

    tidy up

--- se_ss/__init__.py
+++ se_ss/__init__.py
@@ -1,55 +1,63 @@
 import csv
-
-from os.path import join, dirname
 import struct
+from os.path import dirname, join
 from typing import Dict
 
-types={'uint32': (2, ">L") , 'uint16': (1, ">H"), 'String(32)': (16,None),
-       'String(16)': (8,None), 'int16': (1,">h"), 'acc32': (2,">L")}
+types = {
+    "uint32": (2, ">L"),
+    "uint16": (1, ">H"),
+    "String(32)": (16, None),
+    "String(16)": (8, None),
+    "int16": (1, ">h"),
+    "acc32": (2, ">L"),
+}
+
 
 class Entry:
     def __init__(self, e):
-        self.k = e['Name']
-        self.description = e['Description']
-        self.addr = int(e['Address'])
-        self.sz = int(e['Size'])
-        self.unit = e['Units']
-        self.type = e['Type']
-        self.end_addr = self.addr+self.sz
+        self.k = e["Name"]
+        self.description = e["Description"]
+        self.addr = int(e["Address"])
+        self.sz = int(e["Size"])
+        self.unit = e["Units"]
+        self.type = e["Type"]
+        self.end_addr = self.addr + self.sz
         self.raw_offset = None
 
     def replace(self, buff, fragment):
         assert self.sz * 2 >= len(fragment)
         end = self.raw_offset + len(fragment)
-        buff[self.raw_offset:end] = fragment
+        buff[self.raw_offset : end] = fragment
 
     def extract(self, buff):
         raw = self.raw(buff)
         unpack_fmt = types[self.type][1]
         if unpack_fmt is None:
-            end = raw.index(b'\x00')
-            return raw[:end].decode('ascii')
+            end = raw.index(b"\x00")
+            return raw[:end].decode("ascii")
         return struct.unpack(unpack_fmt, raw)[0]
 
     def raw(self, buff):
         end = self.raw_offset + self.sz * 2
-        v = buff[self.raw_offset:end]
+        v = buff[self.raw_offset : end]
         return v
 
     def __str__(self):
         return self.k
 
 
-entries = list(map(Entry, csv.DictReader(open(join(dirname(__file__), "registers.csv")))))
+entries = list(
+    map(Entry, csv.DictReader(open(join(dirname(__file__), "registers.csv"))))
+)
 
 start_addr = entries[0].addr
 
-entries_by_name:Dict[str,Entry] = {}
+entries_by_name: Dict[str, Entry] = {}
 
 for e in entries:
-    assert e.k not in entries_by_name, f'Dublicate {e}'
-    entries_by_name[e.k]=e
-    e.raw_offset = (e.addr - start_addr)*2
+    assert e.k not in entries_by_name, f"Duplicate {e}"
+    entries_by_name[e.k] = e
+    e.raw_offset = (e.addr - start_addr) * 2
     assert e.type in types
     assert types[e.type][0] == e.sz
 
@@ -57,8 +65,5 @@ for e in entries:
 next_addr = None
 for e in entries:
     if next_addr is not None:
-        assert next_addr <= e.addr, f'Field overlap:{e} {e.addr}<{next_addr}'
+        assert next_addr <= e.addr, f"Field overlap:{e} {e.addr}<{next_addr}"
     next_addr = e.end_addr
-
-
-
--- se_ss/tests/parse_tests.py
+++ se_ss/tests/parse_tests.py
@@ -1,23 +1,31 @@
 import pytest
+
 import se_ss
 from se_ss.tests import sample
 
 
-
 @pytest.mark.parametrize(
-    "buff",
-    [
-        sample('at_night.bin'),
-    ],
+    "buff", [sample("at_night.bin"),],
 )
 def test_parse(buff):
-    assert se_ss.entries[0].extract(buff) == 0x53756e53
-    assert se_ss.entries_by_name['C_Manufacturer'].raw(buff) == b'SolarEdge \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    assert se_ss.entries_by_name['C_Model'].raw(buff) == b'SE5000\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    assert se_ss.entries_by_name['C_Version'].raw(buff) == b'0003.2173\x00\x00\x00\x00\x00\x00\x00'
-    assert se_ss.entries_by_name['C_SerialNumber'].raw(buff) == b'F1234567\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    assert se_ss.entries_by_name['C_Manufacturer'].extract(buff) == 'SolarEdge '
-    assert se_ss.entries_by_name['C_Model'].extract(buff) == 'SE5000'
-    assert se_ss.entries_by_name['C_Version'].extract(buff) == '0003.2173'
-    assert se_ss.entries_by_name['C_SerialNumber'].extract(buff) == 'F1234567'
-
+    assert se_ss.entries[0].extract(buff) == 0x53756E53
+    assert (
+        se_ss.entries_by_name["C_Manufacturer"].raw(buff)
+        == b"SolarEdge \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    )
+    assert (
+        se_ss.entries_by_name["C_Model"].raw(buff)
+        == b"SE5000\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    )
+    assert (
+        se_ss.entries_by_name["C_Version"].raw(buff)
+        == b"0003.2173\x00\x00\x00\x00\x00\x00\x00"
+    )
+    assert (
+        se_ss.entries_by_name["C_SerialNumber"].raw(buff)
+        == b"F1234567\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    )
+    assert se_ss.entries_by_name["C_Manufacturer"].extract(buff) == "SolarEdge "
+    assert se_ss.entries_by_name["C_Model"].extract(buff) == "SE5000"
+    assert se_ss.entries_by_name["C_Version"].extract(buff) == "0003.2173"
+    assert se_ss.entries_by_name["C_SerialNumber"].extract(buff) == "F1234567"
